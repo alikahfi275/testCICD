f1cbb235dbc0c0aacb38891fa65cb4d7d9508eef
ali.lubis <ali.lubis@quadrant-si.id>
2025-05-05 13:17:51 +0700

    Test CI/CD 2

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>App</Text>
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
